40932058c3e12af315f0e7dbe71d3f11068570be
Diab1716 <diab.ahmed@etoncollege.org.uk>
2022-01-19 09:55:35 +0000

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Diab, Ahmed (HWTA) on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
